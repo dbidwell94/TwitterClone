4802763b32ea21e28dd60994685da25298c62768
Devin Bidwell <dbidwell94@gmail.com>
2020-09-25 18:34:22 -0700

    initial commit

--- src/main/java/com/biddydev/twitterclone/models/User.java
+++ src/main/java/com/biddydev/twitterclone/models/User.java
@@ -18,4 +18,55 @@ public class User
 
     @Column(nullable = false)
     private String password;
+
+    public User()
+    {
+    }
+
+    public User(String email, String handle, String password)
+    {
+        this.email = email;
+        this.handle = handle;
+        this.password = password;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getHandle()
+    {
+        return handle;
+    }
+
+    public void setHandle(String handle)
+    {
+        this.handle = handle;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
 }
